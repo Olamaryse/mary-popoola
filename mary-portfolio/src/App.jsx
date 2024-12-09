2ed37276ee0967074b23d3c1037a0efdc38b4ff9
Mary Popoola <popoolamarimar@gmail.com>
2024-12-09 17:53:41 +0000

    sections

--- mary-portfolio/src/App.jsx
+++ mary-portfolio/src/App.jsx
@@ -1,13 +1,20 @@
-
-import './App.css'
+import './App.css';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
 
 function App() {
-  
-  return 
+  return (
     <>
-    App
-    </>;
-  
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
